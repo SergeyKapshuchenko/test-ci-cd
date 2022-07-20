a238a16670b0ab3ebf55ed1747da6482a7c38403
SergeyKapshuchenko <serg3y1999@gmail.com>
2022-07-20 15:31:16 +0300

    first ci

---
+++ core.py
@@ -0,0 +1,9 @@
+import json
+import os
+
+pwd = "pwd"
+username = "username"
+
+
+def add(x: int, y: int) -> int:
+    return x + y
---
+++ tests/test_ci.py
@@ -0,0 +1,6 @@
+def test_failing():
+    assert 0 == 1
+
+
+def test_successful():
+    assert 1 == 1
